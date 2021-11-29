9b4a0c174ceef2e59ef9a6d05d407830c4f1fada
Ethan <EthanAEdwards5@gmail.com>
2021-11-29 01:46:01 -0600

    Landing Page Needs Responsiveness

--- API/Models/ConnectionString.cs
+++ API/Models/ConnectionString.cs
@@ -5,11 +5,11 @@ namespace API.Models
         public string cs { get; set; }
         public ConnectionString()
         {
-            string server = "en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-            string database = "pdtfkfwr9pv14mn0";
+            string server = "yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+            string database = "n0f49rs3obnijmmo";
             string port = "3306";
-            string userName = "gtokn48w1l8gymjj";
-            string password = "jeew77ht4kgz3cxh";
+            string userName = "ikq3ovpbfvzmta53";
+            string password = "px6dfc5j02dvnzwj";
 
             cs = $@"server={server}; user={userName}; database={database}; port={port}; password={password}";
         }
--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,7 @@ namespace api
     public class Program
     {
         public static void Main(string[] args)
-        {            
+        {
             CreateHostBuilder(args).Build().Run();
         }
 
